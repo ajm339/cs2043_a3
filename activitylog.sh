a66e2d0119377ec289df97aa9d3978950a2f886f
Alex Meyers <ajm339@cornell.edu>
2015-02-17 21:48:55 -0500

    fixed 4

--- activitylog.sh
+++ activitylog.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
-gawk 'BEGIN {print "Start"}
+gawk 'BEGIN {FS = ","}
 	/work/ {work++}
-	/farmers market/ {farmers_market++}
+	/(start work,)/ {start_work+=$3}
+	/(end work,)/ {end_work+=$3}
+	/(farmers market)/ {farmers_market++}
+	/(start farmers market,)/ {start_farmers_market+=$3}
+	/(end farmers market,)/ {end_farmers_market+=$3}
 	/run/ {run++}
-END {print "Work: " work ", Farmers Market: " farmers_market ", Run: " run "." }
+	/(start run,)/ {start_run+=$3}
+	/(end run,)/ {end_run+=$3}
+END {print "Work: " (start_work) ", happened: " work " times, Farmers Market: " (end_farmers_market) " happened: " farmers_market " times, Run: " (end_run-start_run) " happened: " run " times." }
 ' activity_log.csv
