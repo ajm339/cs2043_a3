9ba6f230da66800ed21fb5266529a368417c0f1f
mjkagie <mjk339@cornell.edu>
2015-02-17 21:42:03 -0500

    Started remix, push before entering plane

---
+++ songremix.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# for i in {1..numberOfLinesTotal}
+# if i modulo 2 == 1
+# paste the next line of song1
+# else paste the next line of song2
+# might need indeces for each, or might just need to do int (i/2), deal with possible offset
